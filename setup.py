63bdc11ffe0a8ddfb471bff7400dd99fa90ee4f7
Bill Wolf <wolfey6@gmail.com>
2016-08-16 11:53:13 -0700

    Add setup file for easy installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mesa_reader',
+    version='0.3.0',
+    description='tools for interacting with output from MESA star',
+    author='William M. Wolf',
+    author_email='wolfey6@gmail.com',
+    license='MIT',
+    install_requires=['numpy'],
+    include_package_data=False,
+    zip_safe=False)
