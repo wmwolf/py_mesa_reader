3e94a3d150b7e8eb9949eb4c2d67825e5f770c92
Bill Wolf <wolfwm@uwec.edu>
2024-08-29 15:39:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,14 @@ setup(name='mesa_reader',
     description='tools for interacting with output from MESA star',
     url='http://github.com/wmwolf/py_mesa_reader',
     author='William M. Wolf',
-    author_email='wolfey6@gmail.com',
+    author_email='wolfwm@uwec.edu',
     license='MIT',
     packages=['mesa_reader'],
-    install_requires=['numpy'])
+    install_requires=['numpy'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6'
+    )
